b6fc7fe7afde9d95ba74f5c94ae23cbe62cc1734
Jon Dedman <jonathandedman@googlemail.com>
2024-02-11 21:14:15 +0000

    improve links on smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import "./App.css";
 // import Key from "./Key.jsx";
 // import Keyboard from "./Keyboard";
+import { useState } from "react";
 import Instructions from "./Instructions";
 import GameBoard from "./Gameboard";
+import Navbar from "./Navbar";
 
 function App() {
-	// const [count, setCount] = useState(0);
-
 	return (
 		<div className="w-full h-full bg-background-color">
-			<div className="flex justify-center items-center bg-banner-color h-[8vh] w-full">
-				<h1 className="text-2xl font-bold text-center font-poppins text-background-color">
-					Jon Dedman - Portfolio
-				</h1>
-			</div>
-			<div className="flex justify-around items-center mx-auto bg-banner-color w-full text-background-color p-2">
-				<h2>+447932563406</h2>
-				<h2>jonathandedman@hotmail.com</h2>
-				<h2>
-					{" "}
-					<a
-						href="https://github.com/jondedman"
-						target="_blank"
-						rel="noreferrer"
-						className="underline"
-					>
-						Github
-					</a>
-				</h2>
-				<h2>
-					{" "}
-					<a
-						href="https://linkedin.com/in/jon-dedman"
-						target="_blank"
-						rel="noreferrer"
-						className="underline"
-					>
-						LinkedIn
-					</a>
-				</h2>
-			</div>
+			<Navbar />
 			<Instructions />
 			<div className="flex items-center justify-center h-[calc(100vh - 15vh)] mx-auto w-3/4 bg-background-color xs:w-full xs:h-full">
 				<GameBoard />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,67 @@
-function Navbar() {
+import React, { useState } from "react";
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<div className="grid grid-cols-3 grid-rows-3">
-			<div className="col-start-2 row-start-2 col-span-3 row-span-1">
-				<h1 className="text-3xl ">Jon Dedman portfolio</h1>
+		<div>
+			<div className="flex justify-center items-center bg-banner-color h-[8vh] w-full">
+				<button
+					className="p-1 md:hidden bg-background-color rounded-lg absolute left-4"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						className="h-4 w-4"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M4 6h16M4 12h16M4 18h16"
+						/>
+					</svg>
+				</button>
+				<h1 className="md:text-2xl xs:text-lg font-bold text-center font-poppins text-background-color">
+					Jon Dedman - Portfolio
+				</h1>
+			</div>
+
+			<div
+				className={`fixed top-0 left-0 w-fit h-fit bg-banner-color xs:opacity-60 md:opacity-100 text-background-color z-10 transform transition-transform duration-200 ease-in-out ${
+					isOpen ? "translate-x-0" : "-translate-x-full"
+				} md:relative md:translate-x-0 md:w-auto md:h-auto xs:flex-col xs:items-start text-background-color md:text-lg md:justify-around mx-auto p-2 xs:text-xs xs:justify-start xs:absolute xs:top-16 md:top-0`}
+			>
+				<div className="md:flex md:flex-row md:justify-center md:items-start ">
+					<h2 className="xs:my-3 xs:mx-2">+447932563406</h2>
+					<h2 className="xs:my-3 xs:mx-2">jonathandedman@hotmail.com</h2>
+					<h2 className="xs:my-3 xs:mx-2 hover:text-blue-800">
+						<a
+							href="https://github.com/jondedman"
+							target="_blank"
+							rel="noreferrer"
+							className="underline active:text-blue active:font-bold"
+						>
+							Github
+						</a>
+					</h2>
+					<h2 className="xs:my-3 xs:mx-2 hover:text-blue-800">
+						<a
+							href="https://linkedin.com/in/jon-dedman"
+							target="_blank"
+							rel="noreferrer"
+							className="underline"
+						>
+							LinkedIn
+						</a>
+					</h2>
+				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 export default Navbar;
